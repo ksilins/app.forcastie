9ad0db8de3f620d58b6c647855462a3eac00010f
Kaspars Silins <kaspars.silins@ask.fm>
2020-04-19 00:19:07 +0300

    added screenshots to failed scenarios. Added slf4j-nop dependency to suppress console errors

--- src/test/java/app/forcastie/hooks/CucumberHooks.java
+++ src/test/java/app/forcastie/hooks/CucumberHooks.java
@@ -1,39 +1,23 @@
 package app.forcastie.hooks;
 
 import app.forcastie.pages.BasePage;
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
 import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-
-/**
- * Cucumber hooks won't be used in presentation to speed up the process.
- * Left commented for future normal testing flow
- */
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class CucumberHooks extends BasePage {
 
-//    @Before
-//    public static void setUp() throws MalformedURLException {
-//        DesiredCapabilities capability = new DesiredCapabilities();
-//
-//        capability.setCapability(CapabilityType.VERSION,"9");
-//        capability.setCapability("app", "/Users/user/IdeaProjects/forcastie/src/test/resources/app.forcastie/apk/app.apk");
-//        capability.setCapability("deviceName", "S8");
-//        capability.setCapability("platformName", "Android");
-//        capability.setCapability("appPackage", "cz.martykan.forecastie");
-//        capability.setCapability("appActivity", "cz.martykan.forecastie.activities.SplashActivity");
-//
-//        driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capability);
-//    }
-//
-//    @After
-//    public static void tearDown() {
-//        if (driver != null) driver.quit();
-//    }
+    @After
+    public void embedScreenshot(Scenario scenario) {
+        if (scenario.isFailed()) {
+            try {
+                byte[] screenshot = ((TakesScreenshot) driver)
+                        .getScreenshotAs(OutputType.BYTES);
+                scenario.embed(screenshot, "image/png");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
